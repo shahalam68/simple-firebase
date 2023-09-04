9fa6b4ea3c0d308789a8e4087deb7399bf64f07b
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-09-04 23:02:14 +0600

    Sign Out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import React, { useState } from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
 import app from '../../firebase/firebase.init';
 const Login = () => {
+    const [user, setUser] = useState(null);
     const auth = getAuth(app);
     console.log(app);
     const provider = new GoogleAuthProvider;
 
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth,provider)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error => {
-            console.log('error' , error.message);
-        })
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
+            })
+            .catch(error => {
+                console.log('error', error.message);
+            })
     }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                setUser(null);
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Google login</button>
+
+            {
+                user ?
+
+                    <button onClick={handleSignOut}>SignOut</button> :
+                    <button onClick={handleGoogleSignIn}>Google login</button>
+            }
+            {
+                user && <div>
+                    <h3>User:{user.displayName}</h3>
+                    <p>Email: {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
 
-export default Login;<h2>login</h2>
+export default Login; <h2>login</h2>
